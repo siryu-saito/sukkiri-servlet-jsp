a9f295e276307d548a42339c8ffd39bfdcddf413
siryu-saito <saito.s.ryu@gmail.com>
2022-02-18 07:32:06 +0900

    フルーツの値段はいくらか表示させる

--- lesson/lesson2/src/model/HealthCheck.java
+++ lesson/lesson2/src/model/HealthCheck.java
@@ -1,13 +1,14 @@
 package model;
 
 import java.io.IOException;
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import model.Health;
+import model.HealthCheckLogic;
 
 @WebServlet("/HealthCheck")
 public class HealthCheck extends HttpServlet {
